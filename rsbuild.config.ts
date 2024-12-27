177a01ceeb6a914e1698e59e5876d5093093b04f
Fatih Altinok <fatihaltinok@live.com>
2024-12-28 00:51:34 +0300

    Make installable

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,6 +4,12 @@ import { pluginReact } from "@rsbuild/plugin-react";
 export default defineConfig({
   html: {
     title: "Quarto",
+    tags: [
+      {
+        tag: "link",
+        attrs: { rel: "manifest", href: "manifest.json" },
+      },
+    ],
   },
   plugins: [pluginReact()],
 });
